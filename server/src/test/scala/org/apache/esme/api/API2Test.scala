58903bf777f8fe6a30827327c415978f52aba8bd
Ethan Jewett <esjewett@gmail.com>
2009-11-26 12:45:56 -0600

    Added a bunch of tests for response codes, many of which fail because we're returning 200 responses when we should be returning 404 or 403s

--- server/src/test/scala/org/apache/esme/api/API2Test.scala
+++ server/src/test/scala/org/apache/esme/api/API2Test.scala
@@ -78,28 +78,44 @@ object Api2Specs extends Specification with TestKit {
 	}
 	
 	"/session GET" in {
-	  for {
-	    session <- post_session
-        session_response <- session.get("session")
-	  } {
-	    session_response.code must be equalTo 200
-	    ( session_response.xml \ "session" \ "user" \ "id").text must be equalTo(theUser.id.toString)
+	  "with valid session" in {
+	    for {
+	      session <- post_session
+          session_response <- session.get("session")
+	    } {
+	      session_response.code must be equalTo 200
+	      ( session_response.xml \ "session" \ "user" \ "id").text must be equalTo(theUser.id.toString)
+	    }
+	  }
+	
+	  "with no session returns 404 (not found)" in {
+	    for(session_res <- get("session")) {
+	      session_res.code must be equalTo 404
+	    }
 	  }  
     }
 
     "/session DELETE" in {
-      for {
-        session <- post_session
-        session_del_response <- session.delete("session")
-        //session_response <- session.get("session")
-      } {
-        session_del_response.code must be equalTo 200
-        //session_response.code must be equalTo(404)
+      "with valid session" in {
+        for {
+          session <- post_session
+          session_del_response <- session.delete("session")
+          //session_response <- session.get("session")
+        } {
+          session_del_response.code must be equalTo 200
+          //session_response.code must be equalTo(404)
+        }
       }
+
+	  "with no session returns 404 (not found)" in {
+	    for(session_res <- delete("session")) {
+	      session_res.code must be equalTo 404
+	    }
+	  }
     }	
 
     "/users GET" in {
-      "Valid session have a response code of 200" in {     
+      "with valid session" in {     
         for {
           session <- post_session
           users <- session.get("users")
@@ -107,33 +123,114 @@ object Api2Specs extends Specification with TestKit {
           users.code must be equalTo 200
         }
       }
+
+	  "with no session returns 403 (forbidden)" in {
+	    for(session_res <- get("users")) {
+	      session_res.code must be equalTo 403
+	    }
+	  }
     }
 
     "/user/messages GET" in {
-      for {
-        session <- post_session
-        mess_res <- session.get("user/messages")
-      } {
-        mess_res.code must be equalTo 200
+      "with valid session" in {
+        for {
+          session <- post_session
+          mess_res <- session.get("user/messages")
+        } {
+          mess_res.code must be equalTo 200
+        }
       }
+
+	  "with no session returns 403 (forbidden)" in {
+	    for(session_res <- get("user/messages")) {
+	      session_res.code must be equalTo 403
+	    }
+	  }
     }
 
     "/user/messages?history=10 GET" in {
-      for {
-        session <- post_session
-        res <- session.get("user/messages?history=10")
-      } {
-        res.code must be equalTo 200
+      "with valid session" in {
+        for {
+          session <- post_session
+          res <- session.get("user/messages?history=10")
+        } {
+          res.code must be equalTo 200
+        }                     
       }
+
+      "with no session returns 403 (forbidden)" in {
+	    for(session_res <- get("user/messages?history=10")) {
+	      session_res.code must be equalTo 403
+	    }
+	  }
     }
 
-    "/user/messages?timeout=2" in {
-      for {
-        sess <- post_session
-        res <- sess.get("user/messages?timeout=2")
-      } {
-        res.code must be equalTo 200
+    "/user/messages?timeout=2 GET" in {
+      "with valid session" in {
+        for {
+          sess <- post_session
+          res <- sess.get("user/messages?timeout=2")
+        } {
+          res.code must be equalTo 200
+        }          
       }
-    }                
+
+      "with no session returns 403 (forbidden)" in {
+	    for(session_res <- get("user/messages?timeout=2")) {
+	      session_res.code must be equalTo 403
+	    }
+	  }
+    }
+
+    "/user/followees GET" in {
+      "with valid session" in {
+        for {
+          sess <- post_session
+          res <- sess.get("user/followees")
+        } {
+          res.code must be equalTo 200
+        }          
+      }
+
+      "with no session returns 403 (forbidden)" in {
+	    for(session_res <- get("user/followees")) {
+	      session_res.code must be equalTo 403
+	    }
+	  }
+    }
+
+    "/user/followees POST" in {
+      "with valid session" in {
+        for {
+          sess <- post_session
+          res <- sess.post("user/followees", "userId" -> "1")
+        } {
+          res.code must be equalTo 200
+        }          
+      }
+
+      "with no session returns 403 (forbidden)" in {
+	    for(res <- post("user/followees", "userId" -> "1")) {
+	      res.code must be equalTo 403
+	    }
+	  }
+    } 
+
+    "/user/followers GET" in {
+      "with valid session" in {
+        for {
+          sess <- post_session
+          res <- sess.get("user/followers")
+        } {
+          res.code must be equalTo 200
+        }          
+      }
+
+      "with no session returns 403 (forbidden)" in {
+	    for(session_res <- get("user/followers")) {
+	      session_res.code must be equalTo 403
+	    }
+	  }
+    }              
   }  
 }
